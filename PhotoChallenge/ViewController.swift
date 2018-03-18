d86a3010e7f609c2b633d326f7c8516ac78b71ed
Sebastian Prokesch <sebastian.prokesch@gmail.com>
2018-03-18 21:43:56 +0100

    Initial Commit

---
+++ PhotoChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoChallenge
+//
+//  Created by Sebastian Prokesch on 18.03.18.
+//  Copyright © 2018 Sebastian Prokesch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
